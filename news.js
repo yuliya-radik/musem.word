7441a36f94509435c0ff49c52e02f73217898171
sleshua <163445112+sleshua@users.noreply.github.com>
2025-06-03 20:30:33 +0200

    main

---
+++ news.js
@@ -0,0 +1,8 @@
+function toggleText() {
+    var text = document.getElementById("text");
+    if (text.style.display === "none") {
+        text.style.display = "block"; // Показати текст
+    } else {
+        text.style.display = "none"; // Приховати текст
+    }
+}
